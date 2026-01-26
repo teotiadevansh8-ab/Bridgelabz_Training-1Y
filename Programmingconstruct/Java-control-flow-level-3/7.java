c4958d75f63608e78e16416a4bd77e3b4e663c45
teotiadevansh8-ab <teotiadevansh8@gmail.com>
2026-01-26 18:18:46 +0530

    weight and height input

--- Programmingconstruct/Java-control-flow-level-3/7.java
+++ Programmingconstruct/Java-control-flow-level-3/7.java
@@ -4,8 +4,8 @@ class BMI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double weight = sc.nextDouble(); // kg
-        double heightCm = sc.nextDouble(); // cm
+        double weight = sc.nextDouble(); 
+        double heightCm = sc.nextDouble(); 
 
         double heightM = heightCm / 100;
         double bmi = weight / (heightM * heightM);
